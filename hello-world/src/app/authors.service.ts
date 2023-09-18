a0021cead8b1d0ef5943ec4564e0a2d73048052f
ageek <re.90@live.com>
2023-09-18 12:56:49 +0530

    hello world - authos app

---
+++ hello-world/src/app/authors.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorsService {
+
+  getAuthors() {
+    return ["Author1", "Author2", "Author3", "Author4", "Author5", "Author6", "Author7"];
+    
+  }
+}
